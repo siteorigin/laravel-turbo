e7ce004f981bb2f8a8b9f693d7299dc99f99817d
Greg Priday <greg@siteorigin.com>
2020-12-05 12:28:12 +0200

    wip

--- src/TurboServiceProvider.php
+++ src/TurboServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace SiteOrigin\Turbo;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class TurboServiceProvider extends ServiceProvider
@@ -25,5 +26,7 @@ class TurboServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../dist/js/turbo.js' => public_path('vendor/turbo/turbo.js'),
         ]);
+
+        Blade::directive('turbo_script', fn() => '<script src="' . url('vendor/turbo/turbo.js') . '" defer></script>');
     }
 }
